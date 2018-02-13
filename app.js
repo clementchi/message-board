95558f101832566b7e45af87253f4427096239b4
CLEMENT CHI <CLEMENTCHI@YAHOO.COM>
2018-02-12 22:38:58 -0800

    update to dynamic port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fs = require('fs');
 const app = express()
-const port = process.env.port || 7085;
+const port = process.env.PORT || 7085;
 const https = require('https');
 
 app.use(express.static(__dirname + '/build'));
