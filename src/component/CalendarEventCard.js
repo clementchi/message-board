7106a1975f3f399cd9ed4cd145c80b31560f9bf0
CLEMENT CHI <CLEMENTCHI@YAHOO.COM>
2018-02-13 09:02:32 -0800

    new build

--- src/component/CalendarEventCard.js
+++ src/component/CalendarEventCard.js
@@ -6,7 +6,9 @@ import * as weatherAction from '../reduxActionReducer/weatherActionReducer';
 
 class CalendarEventCard extends Component {
     componentDidMount(){
-        this.getData();
+      if (this.props.event.location){
+        this.getData();          
+      }
     }
     getData(){
         this.props.getTripDuration(this.props.context, this.props.event.location);
@@ -17,49 +19,54 @@ class CalendarEventCard extends Component {
     }       
     render(){
         let eventInfo = this.props.event;
-        let weatherResponse = weatherAction.resolveWeatherFromProps(this.props, this.props.event.location.replace(/#/g,''));
-        let tripKey = tripAction.getTripKey(this.props.context.latitude, this.props.context.longitude, this.props.event.location);
-        let tripResponse = tripAction.resolveTripResponseFromProp(this.props, tripKey);        
-        if (eventInfo && tripResponse && weatherResponse){
-            let duration = tripResponse.rows[0].elements[0].duration_in_traffic.text;
-            let trafficDelay = (tripResponse.rows[0].elements[0].duration_in_traffic.value - tripResponse.rows[0].elements[0].duration.value) / 60
-            let reportTime = new Date().toLocaleTimeString();
+        let duration = '-';
+        let trafficDelay = '-';
+        let reportTime = new Date().toLocaleTimeString();
+        let temperature;
+        let conditionText = '-';
+        let delayStyle;
+        let location = 'Unknown location';
 
-            let temperature = weatherResponse.temp;
-            let conditionText = weatherResponse.text;
-        
-
-            let delayStyle;
-            if (trafficDelay > 20){
-              delayStyle = 'alert-danger';
-            }
-            else if (trafficDelay > 10){
-              delayStyle = 'alert-warn';
-            }
-            else if (trafficDelay > 5){
-              delayStyle = 'alert-info';
-            }
-            else {
-              delayStyle = 'alert-success';
-            }
-        
-            return (
-              <section className="card">     
-                <h3>{eventInfo.summary}</h3>
-                <p><i className="fa fa-map-marker"></i> {eventInfo.location}</p>
-                <div className="content">
-                  <div className="column">
-                    <h1 className={delayStyle}>{duration}</h1>
-                  </div>
-                  <div className="column">
-                    <h3>{conditionText} {temperature} F</h3>
-                  </div>                        
-                </div>
-                <p>{reportTime}</p>     
-              </section>
-            );
-        }
-        return null;
+        if (eventInfo.location){
+          location = eventInfo.location;
+          let weatherResponse = weatherAction.resolveWeatherFromProps(this.props, eventInfo.location.replace(/#/g,''));
+          let tripKey = tripAction.getTripKey(this.props.context.latitude, this.props.context.longitude, eventInfo.location);
+          let tripResponse = tripAction.resolveTripResponseFromProp(this.props, tripKey);        
+          if (eventInfo && tripResponse && weatherResponse){
+              duration = tripResponse.rows[0].elements[0].duration_in_traffic.text;
+              trafficDelay = (tripResponse.rows[0].elements[0].duration_in_traffic.value - tripResponse.rows[0].elements[0].duration.value) / 60
+              temperature = weatherResponse.temp;
+              conditionText = weatherResponse.text;
+              
+              if (trafficDelay > 20){
+                delayStyle = 'alert-danger';
+              }
+              else if (trafficDelay > 10){
+                delayStyle = 'alert-warn';
+              }
+              else if (trafficDelay > 5){
+                delayStyle = 'alert-info';
+              }
+              else {
+                delayStyle = 'alert-success';
+              }
+          }  
+        }  
+        return (
+          <section className="card">     
+            <h3>{eventInfo.summary}</h3>
+            <p><i className="fa fa-map-marker"></i> {location}</p>
+            <div className="content">
+              <div className="column">
+                <h1 className={delayStyle}>{duration}</h1>
+              </div>
+              <div className="column">
+                <h3>{conditionText} {temperature} F</h3>
+              </div>                        
+            </div>
+            <p>{reportTime}</p>     
+          </section>
+        );              
     }
 }
 
